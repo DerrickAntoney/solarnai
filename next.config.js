99fa190ab5d3403e3aad40f0aeadba3e9135adf8
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2025-01-19 12:21:34 +0300

    Fixes image error

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'fkmgsjkgkaewsfvdjccq.supabase.co', // Replace with your Supabase domain
+          port: '',
+          pathname: '/storage/v1/object/sign/solarpics/**', // Adjust based on your storage path
+        },
+      ],
+    },
+  };
+  
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fkmgsjkgkaewsfvdjccq.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/sign/solarpics/**',
+        search: '',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+export default config;
